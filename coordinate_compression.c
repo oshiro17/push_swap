af571f3fd5aa13244e017b08bdc321b7dea0aa55
oshro17 <nonokapiano@icloud.com>
2022-11-30 00:47:16 +0900

    intmax に対応させた

--- coordinate_compression.c
+++ coordinate_compression.c
@@ -6,48 +6,53 @@
 /*   By: panti <panti@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 09:40:49 by panti             #+#    #+#             */
-/*   Updated: 2022/11/30 00:13:04 by panti            ###   ########.fr       */
+/*   Updated: 2022/11/30 00:43:08 by panti            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-int	*coordinate_compression(int argc, const char **str)
+int *str_to_num(int argc,const char **str)
 {
+	int *num;
+	int elem;
 	int index;
-	int	*num;
-	int i;
-	int k;
-
-	i = 0;
-	k = 0;
-	while (str[k])
-	{
-		k++;
-	}
-    if (argc == 2)
+	
+	elem = 0;
+	if(argc == 2)
 	{
 		index = 0;
-		num = malloc(sizeof(int) * (size_t)k);
+		while(str[elem])
+			elem++;
+		num = malloc(sizeof(int) * (size_t)elem);		
 	}
 	else
-	{
+	{	
 		index = 1;
-		num = malloc(sizeof(int) * ((size_t)argc - 1));
+		num = malloc(sizeof(int)*argc);
 	}
-	while ((k > index) && *(str[index]))
+	elem = 0;
+	while(str[index])
 	{
-		num[i] = ft_atol(str[index]);
+		num[elem] = ft_atol(str[index]);
 		index++;
+		elem++;
+	}
+	return(num);
+}
+
+void	*coordinate_compression(int argc, const char **str)
+{
+	int	*num;
+	int i;
+
+	i=0;
+	num = str_to_num(argc,str);
+	while(num[i])
+	{
+		printf("{%d}\n",num[i]);
 		i++;
 	}
-	i = 0;
-	//to_check
-	// while(num[i])
-	// {
-	// 	printf("{%d}\n",num[i]);
-	// 	i++;
-	// }
 	return (num);
 }
--- main.c
+++ main.c
@@ -3,36 +3,10 @@
 
 int	main(int argc, char const *argv[])
 {
-	// t_stack		*stack_a;
-//	t_stack		*stack_b;
-	const char		**str;
-	// size_t      index;
-	// int		i;
-	// int 	k;
-	// int		*num;
+	const char	**str;
 
-
-	// k=0;
-	// i = 0;
-	// if (argc < 2)
-	// 	exit(1);
-	// else if (argc == 2)
-	// 	index = 0;
-	// else
-	// 	index = 1;
 	str = check_args(argc, argv);
-
-
- coordinate_compression(argc, str);
-// to_check
-  	// while(k < 10)
-	// {
-	// 	printf("%d\n",num[i]);
-	// 	i++;
-	// 	k++;
-	// }
-//to_check
-
+	coordinate_compression(argc, str);
 // 	put_stack(argc, (char **)str, &stack_a);
 
     return (0);
