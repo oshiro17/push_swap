1f1fd5bd5731543156884557b9b5a1171e892a04
oshro17 <nonokapiano@icloud.com>
2022-12-02 18:08:59 +0900

    座標圧縮完了！

--- coordinate_compression.c
+++ coordinate_compression.c
@@ -6,53 +6,84 @@
 /*   By: panti <panti@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 09:40:49 by panti             #+#    #+#             */
-/*   Updated: 2022/11/30 00:43:08 by panti            ###   ########.fr       */
+/*   Updated: 2022/12/02 18:08:15 by panti            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-int *str_to_num(int argc,const char **str)
+int	*compression(int *num,int elem)
+{
+	int	i;
+	int j;
+	int number;
+	int *new_num;
+
+	new_num = malloc(sizeof(int)*elem);
+	number = 0;
+	i = 0;
+	while(i < elem)
+	{
+		j = 0;
+		while (j < elem)
+		{
+			if (num[i] > num[j])
+				number++;
+			j++;
+		}
+		new_num[i] = number;
+		number = 0;
+		i++;
+	}
+	num = new_num;
+	free(new_num);
+	return(num);
+}
+
+int *str_to_num(int argc,const char **str,int elem)
 {
 	int *num;
-	int elem;
 	int index;
-	
-	elem = 0;
+	int k;
+
 	if(argc == 2)
-	{
 		index = 0;
-		while(str[elem])
-			elem++;
-		num = malloc(sizeof(int) * (size_t)elem);		
-	}
 	else
-	{	
 		index = 1;
-		num = malloc(sizeof(int)*argc);
-	}
-	elem = 0;
+	num = malloc(sizeof(int) * (size_t)elem);	
+	k = 0;
 	while(str[index])
 	{
-		num[elem] = ft_atol(str[index]);
+		num[k] = ft_atol(str[index]);
 		index++;
-		elem++;
+		k++;
 	}
 	return(num);
 }
 
-void	*coordinate_compression(int argc, const char **str)
+void	coordinate_compression(int argc, const char **str)
 {
 	int	*num;
 	int i;
+	int elem;
 
 	i=0;
-	num = str_to_num(argc,str);
-	while(num[i])
+	elem = 0;
+	if(argc == 2)
+	{
+		while(str[elem])
+			elem++;	
+	}
+	else
+		elem = argc - 1;
+	num = str_to_num(argc,str,elem);
+	num = compression(num,elem);
+	i = 0;
+	while(i<elem)
 	{
 		printf("{%d}\n",num[i]);
 		i++;
 	}
-	return (num);
+	return;
 }
