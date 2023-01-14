d6e7a28eba18c040a34cda1659cca3bbe0298cc7
oshro17 <nonokapiano@icloud.com>
2023-01-14 19:39:07 +0900

    saikou

--- sort_stack.c
+++ sort_stack.c
@@ -6,12 +6,44 @@
 /*   By: panti <panti@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 16:21:43 by panti             #+#    #+#             */
-/*   Updated: 2023/01/14 17:22:17 by panti            ###   ########.fr       */
+/*   Updated: 2023/01/14 19:38:27 by panti            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
+
+void rra(t_stack **a)
+{
+	t_stack *go_first;
+	t_stack *first_now;
+
+	first_now = (*a)->next;
+	go_first = (*a)->previous;
+
+	(*a)->previous = go_first->previous;
+	(*a)->next = go_first;
+	(*a)->previous->next = (*a);
+	go_first->next = first_now;
+	go_first->previous = (*a);
+	first_now->previous = go_first; 
+}
+
+void ra(t_stack **a)
+{
+	t_stack *go_last;
+	t_stack *last_now;
+
+	go_last = (*a)->next;
+	last_now = (*a)->previous;
+
+	(*a)->next = go_last->next;
+	(*a)->previous = go_last;
+	(*a)->next->previous = (*a);
+	go_last->previous = last_now;
+	go_last->next = (*a);
+	last_now->next = go_last;
+}
 
 void sa(t_stack **a_stack)
 {
@@ -34,8 +66,6 @@ void    pb(t_stack **a, t_stack **b)
     t_stack *push;
 
     push = (*a)->next;
-
-// printf("pointer B::%p\n",(*b)->next);    
     push->next->previous = push->previous;
     push->previous->next = push ->next;
     push->previous = (*b);
@@ -57,12 +87,27 @@ void    sort_stack(t_stack **a_stack, t_stack **b_stack)
 {
     int i = 0; 
     t_stack *tmp;
+    // rra(a_stack);
+	// sa(a_stack);
+    //pb(a_stack,b_stack);
 
-    // pb(a_stack,b_stack);
-    // pb(a_stack,b_stack);
-    // printf("pointer B::%p\n",(*b_stack)->next);
-    // printf("pointer B::%p\n",(*b_stack)->next);
-    // printf("pointer B::%p\n",(*b_stack)->next);
+	sa(a_stack);//sa
+	pb(a_stack,b_stack);//pb
+	pb(a_stack,b_stack);//pb
+	pb(a_stack,b_stack);//pb
+	ra(a_stack);//ra
+	ra(b_stack);//rb
+	rra(a_stack);//rra
+	rra(b_stack);//rrb
+	sa(a_stack);//sa
+	pb(b_stack,a_stack);//pa
+	pb(b_stack,a_stack);//pa
+	pb(b_stack,a_stack);//pa
+
+
+
+
+
     tmp = (*a_stack);
     while(i < 15)
     {
@@ -70,12 +115,12 @@ void    sort_stack(t_stack **a_stack, t_stack **b_stack)
         tmp = tmp->next;
         i++;
     }
-    i = 0;
-    tmp = (*b_stack);
-    while(i < 15)
-    {
-        printf("b:::%d\n",tmp->num);
-        tmp=tmp->next;
-        i++;
-    }
+    // i = 0;
+    // tmp = (*b_stack);
+    // while(i < 15)
+    // {
+    //     printf("b:::%d\n",tmp->num);
+    //     tmp=tmp->next;
+    //     i++;
+    // }
 }
