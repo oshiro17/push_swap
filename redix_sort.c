13628bdf2e476044824ab1ef55e648eb7bddee37
oshro17 <nonokapiano@icloud.com>
2023-01-27 20:49:25 +0900

    kkk

--- redix_sort.c
+++ redix_sort.c
@@ -3,14 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   redix_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
+/*   By: panti <panti@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 21:34:49 by panti             #+#    #+#             */
-/*   Updated: 2023/01/27 04:54:44 by noshiro          ###   ########.fr       */
+/*   Updated: 2023/01/27 20:47:25 by panti            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int get_log(int node_num)
+{
+	int i;
+
+	i = 0;
+	while (0 < node_num)
+	{
+		node_num /= 2;
+		i++;
+	}
+	return (i);
+}
 
 void radix_sort(t_stack **a, t_stack **b, int node_num)
 {
@@ -21,15 +34,14 @@ void radix_sort(t_stack **a, t_stack **b, int node_num)
 
 	digit = 0;
 	i = 0;
-	
-	while(digit < 32)
+	while(digit < get_log(node_num))
 	{
 		pb_count = 0;
 		while (i < node_num)
 		{
 			if((((*a)->next->num) >> digit & 1) == 1 )
 				ra(a,'a');
-			else if((((*a)->next->num) >> digit & 1) == 0)
+			else 
 			{
 				pb(a,b,'b');
 				pb_count++;
